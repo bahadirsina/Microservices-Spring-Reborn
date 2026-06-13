2778da7ec820eaa171a3335d74b6d15d3077b8bf
bahadirsina <bahadirsinaterzioglu01@gmail.com>
2026-06-14 02:11:23 +0300

    Test Config-Server

---
+++ microservices/product-service/src/main/java/com/turkcell/product_service/ProductsController.java
@@ -0,0 +1,19 @@
+package com.turkcell.product_service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductsController {
+
+
+    @GetMapping()
+    public String Hello() {
+        return "Hello";
+    }
+    
+
+}
